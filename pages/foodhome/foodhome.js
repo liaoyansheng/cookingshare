1da18f5c65fa500d021b9c67dbedf1d3c4ecf277
空心菜 <578774356@qq.com>
2019-01-17 14:42:19 +0800

    第一次上传

---
+++ pages/foodhome/foodhome.js
@@ -0,0 +1,129 @@
+// pages/foodhome/foodhome.js
+var time = 0;
+var touchDot = 0;//触摸时的原点
+var interval = "";
+var flag_hd = true;
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      '/image/adpic/01.jpg',
+      '/image/adpic/02.jpg',
+      '/image/adpic/03.jpg',
+    ],
+    swiperIndex: 0
+  },
+
+
+  bindchange(e) {
+    this.setData({
+      swiperIndex: e.detail.current
+    })
+  },
+
+  // 触摸开始事件
+  touchStart: function (e) {
+    touchDot = e.touches[0].pageX; // 获取触摸时的原点
+    // 使用js计时器记录时间    
+    interval = setInterval(function () {
+      time++;
+    }, 100);
+  },
+
+  // 触摸结束事件
+  touchEnd: function (e) {
+    var touchMove = e.changedTouches[0].pageX;
+    // 向左滑动   
+    if (touchMove - touchDot <= -100 && time < 10 && flag_hd == true) {
+      flag_hd = false;
+      //执行切换页面的方法
+      //console.log("向右滑动");
+      wx.switchTab({
+        url: '../classify/classify'
+      })
+    }
+    // 向右滑动   
+    if (touchMove - touchDot >= 100 && time < 10 && flag_hd == true) {
+      //flag_hd = false;
+      //执行切换页面的方法
+      //console.log("向左滑动");
+      // wx.switchTab({
+      //   url: '../foodhome/foodhome'
+      // })
+    }
+    clearInterval(interval); // 清除setInterval
+    time = 0;
+  },
+  
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    // wx.showTabBarRedDot({
+    //   index: 2,
+    //   success: function (e) {
+    //     console.log("成功");
+    //   }
+    // });
+    wx.setTabBarBadge({
+      index: 2,
+      text: '3'
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    flag_hd = true;    //重新进入页面之后，可以再次执行滑动切换页面代码
+    clearInterval(interval); // 清除setInterval
+    time = 0;
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,122 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+var time = 0;
+var touchDot = 0;//触摸时的原点
+var interval = "";
+var flag_hd = true;
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    var that = this;
+    wx.removeTabBarBadge({
+      index: 2
+    });
+
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (that.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        that.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          that.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  // 触摸开始事件
+  touchStart: function (e) {
+    touchDot = e.touches[0].pageX; // 获取触摸时的原点
+    // 使用js计时器记录时间    
+    interval = setInterval(function () {
+      time++;
+    }, 100);
+  },
+  // 触摸结束事件
+  touchEnd: function (e) {
+    var touchMove = e.changedTouches[0].pageX;
+    // 向左滑动   
+    if (touchMove - touchDot <= -100 && time < 10 && flag_hd == true) {
+      //flag_hd = false;
+      //执行切换页面的方法
+      //console.log("向右滑动");
+      // wx.switchTab({
+      //   url: '../index/index'
+      // })
+    }
+    // 向右滑动   
+    if (touchMove - touchDot >= 100 && time < 10 && flag_hd == true) {
+      flag_hd = false;
+      //执行切换页面的方法
+      //console.log("向左滑动");
+      wx.switchTab({
+        url: '../classify/classify'
+      })
+    }
+    clearInterval(interval); // 清除setInterval
+    time = 0;
+  },
+  /**
+ * 生命周期函数--监听页面显示
+ */
+  onShow: function () {
+    flag_hd = true;    //重新进入页面之后，可以再次执行滑动切换页面代码
+    clearInterval(interval); // 清除setInterval
+    time = 0;
+  },
+  /**
+ * 生命周期函数--监听页面初次渲染完成
+ */
+  onReady: function () {
+    wx.showTabBarRedDot({
+      index: 2,
+      success: function (e) {
+        console.log("成功");
+      }
+    });
+    // wx.hideTabBarRedDot({
+    //   index: 2,
+    //   success: function (e) {
+    //     console.log("成功");
+    //   }
+    // })
+  },
+})
